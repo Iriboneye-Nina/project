891a7ff4ae8f4dfb5a4ac5437ddbbb4602cffc91
IRIBONEYE NINA <ninairiboneye@gmail.com>
2024-02-08 17:25:25 +0200

    add url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-// export const BASE_URL = "http://localhost:5000/api/v1";
-export const BASE_URL = "https://medicare-booking-website.onrender.com/api/v1";
+export const BASE_URL = "https://backendapp-zyic.onrender.com/api/v1";
+// export const BASE_URL = "https://medicare-booking-website.onrender.com/api/v1";
 export const token = localStorage.getItem("token");
